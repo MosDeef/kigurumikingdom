50a31c0e3ce4ec2dac521468b9d5da69b8c2fd26
Gilary Bacnis <gilarybacnis@gmail.com>
2023-02-20 17:45:55 +0900

    modified models

--- app/models/mascot.rb
+++ app/models/mascot.rb
@@ -2,5 +2,5 @@ class Mascot < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   validates :name, :location, presence: true
-  validates :description, length: { minimum: 20 }
+  validates :description, length: { maximum: 20 }
 end
