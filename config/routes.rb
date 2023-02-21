df0b9bab7ffc9c7101aceed94baf39cfc034c471
Jim Deeth <james.deeth@gmail.com>
2023-02-21 14:28:51 +0900

    bookings controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :mascots, only: %i[index show new create]
-  resources :bookings, only: %i[index create update]
+  resources :mascots, only: %i[index show new create] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: %i[index show create update]
   namespace :owner do
     resources :bookings, only: :index
   end
